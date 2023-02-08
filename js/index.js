aad090de61873f18815f8b5888ae9eceb533842a
Damilare Adepoju <108327427+adamilare@users.noreply.github.com>
2023-02-08 16:06:48 +0100

    Finish preliminary setup

---
+++ js/index.js
@@ -0,0 +1,14 @@
+function mobileMenuClicked() {
+  let menu = document.getElementById("mobile-menu-icon");
+  menu.classList.toggle("fa-bars");
+  menu.classList.toggle("fa-cancel");
+  document.getElementById("nav-wrapper").toggleAttribute("hidden");
+}
+
+document
+  .querySelector("#mobile-menu-icon")
+  .addEventListener("click", mobileMenuClicked);
+
+document.querySelectorAll("#mobile-menu-items li").forEach((item) => {
+  item.addEventListener("click", mobileMenuClicked);
+});
